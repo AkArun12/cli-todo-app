4d81fa1b1c50a942fe7e539261b85a748ab0e31c
Arun <Arun.Kathariya@student.oulu.fi>
2026-07-02 23:10:26 +0300

    Add task functionality

---
+++ main.py
@@ -0,0 +1,50 @@
+tasks=[]
+def show_menu():
+    print("\n=========================")
+    print("CLI TO-DO APP")
+    print("==========================")
+    print("1. Add Task\n")
+    print("2. View Task\n")
+    print("3. Delete Task\n")
+    print("4. Mark Complete\n")
+    print("5. Exit\n")
+
+
+    #Function to Add Tasks.
+
+def add_task(tasks):
+    title=input("Enter the task: ")
+    task={
+            "id":len(tasks)+1,
+            "title":title,
+            "completed":False
+        }
+    tasks.append(task)
+    print("Task added successfully!")
+
+def main():
+    while True:
+        show_menu()
+        try:
+            choice=int(input("Enter ypur choice: "))
+            if choice == 1:
+                add_task(tasks)
+            elif choice == 2:
+                print("You selected : View Task")
+            elif choice == 3:
+                print("You selected : Delete Task")
+            elif choice == 4:
+                print("You selected : Mark Complete") 
+            elif choice == 5:
+                print("You selected : Exit")
+                break
+            else:
+                print("Invalid input.")
+        except ValueError:
+            print("Please enter a number between 1 and 5.")
+
+if __name__ == "__main__":
+    main()
+    
+
+
